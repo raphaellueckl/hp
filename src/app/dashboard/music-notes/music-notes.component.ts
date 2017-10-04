aa948e4af1132edd5a6591f0560001ce551df4bb
codepleb <codepleb4@gmail.com>
2017-10-04 13:57:02 -0700

    Improved random algorithm (always new element), Layout changes

--- src/app/dashboard/music-notes/music-notes.component.ts
+++ src/app/dashboard/music-notes/music-notes.component.ts
@@ -11,9 +11,6 @@ import {NoteTuple} from './single-music-note/note-tuple';
 })
 export class MusicNotesComponent implements OnInit {
 
-  // readonly tunes = ['a#', 'a', 'a_#', 'a_', 'b', 'b_', 'c#', 'c', 'c_#', 'c_', 'd#', 'd', 'd_#', 'd_', 'e', 'e_', 'f#',
-  //   'f', 'f_', 'g#', 'g'];
-
   readonly tunes = [
     new NoteTuple('A#', 'assets/images/music-notes/a0.png'),
     new NoteTuple('A', 'assets/images/music-notes/a.png'),
@@ -50,27 +47,6 @@ export class MusicNotesComponent implements OnInit {
   maxInterval: number = 5000;
 
   constructor() {
-    // this.map.set('a#', new NoteTuple('A#', 'assets/images/music-notes/a0.png'));
-    // this.map.set('a', new NoteTuple('A', 'assets/images/music-notes/a.png'));
-    // this.map.set('a_#', new NoteTuple('A#', 'assets/images/music-notes/a_0.png'));
-    // this.map.set('a_', new NoteTuple('A', 'assets/images/music-notes/a_.png'));
-    // this.map.set('b', new NoteTuple('B', 'assets/images/music-notes/b.png'));
-    // this.map.set('b_', new NoteTuple('B', 'assets/images/music-notes/b_.png'));
-    // this.map.set('c#', new NoteTuple('C#', 'assets/images/music-notes/c0.png'));
-    // this.map.set('c', new NoteTuple('C', 'assets/images/music-notes/c.png'));
-    // this.map.set('c_#', new NoteTuple('C#', 'assets/images/music-notes/c_0.png'));
-    // this.map.set('c_', new NoteTuple('C', 'assets/images/music-notes/c_.png'));
-    // this.map.set('d#', new NoteTuple('D#', 'assets/images/music-notes/d0.png'));
-    // this.map.set('d', new NoteTuple('D', 'assets/images/music-notes/d.png'));
-    // this.map.set('d_#', new NoteTuple('D#', 'assets/images/music-notes/d_0.png'));
-    // this.map.set('d_', new NoteTuple('D', 'assets/images/music-notes/d_.png'));
-    // this.map.set('e', new NoteTuple('E', 'assets/images/music-notes/e.png'));
-    // this.map.set('e_', new NoteTuple('E', 'assets/images/music-notes/e_.png'));
-    // this.map.set('f#', new NoteTuple('F#', 'assets/images/music-notes/f0.png'));
-    // this.map.set('f', new NoteTuple('F', 'assets/images/music-notes/f.png'));
-    // this.map.set('f_', new NoteTuple('F', 'assets/images/music-notes/f_.png'));
-    // this.map.set('g#', new NoteTuple('G#', 'assets/images/music-notes/g0.png'));
-    // this.map.set('g', new NoteTuple('G', 'assets/images/music-notes/g.png'));
   }
 
   ngOnInit() {
@@ -78,12 +54,14 @@ export class MusicNotesComponent implements OnInit {
 
   private randomChar(): NoteTuple {
     return this.tunes[Math.floor(Math.random() * this.tunes.length)];
-    // return this.tunes[0];
   }
 
   onStart() {
     clearInterval(this.spitRandomChars);
-    this.currentNote = this.randomChar();
+    const previousNote = this.currentNote;
+    while (this.currentNote === previousNote) {
+      this.currentNote = this.randomChar();
+    }
     console.log(this.currentNote)
     this.spitRandomChars = setInterval(() => {
       this.currentNote = this.randomChar();
