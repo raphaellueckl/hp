1df25a4a3a439ff9d213822b0e6f283391e65f01
codepleb <codepleb4@gmail.com>
2017-10-14 00:08:20 -0700

    Make google adsense work

--- src/app/dashboard/adverts/adverts.component.ts
+++ src/app/dashboard/adverts/adverts.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-adverts',
   templateUrl: './adverts.component.html',
   styleUrls: ['./adverts.component.css']
 })
-export class AdvertsComponent implements OnInit {
+export class AdvertsComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit(): void {
+    try {
+      (window['adsbygoogle'] = window['adsbygoogle'] || []).push({});
+    } catch (e) {
+      console.error('Ad error');
+    }
   }
 
 }
